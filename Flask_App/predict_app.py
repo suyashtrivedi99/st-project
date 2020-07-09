eee83d7ab7896c2a89a8f98aba3c96d1265468ca
Suyash Trivedi <37772942+suyashtrivedi99@users.noreply.github.com>
2020-07-09 21:05:53 +0530

    fixed tensorflow GPU bugs and CORs policy error

--- Flask_App/predict_app.py
+++ Flask_App/predict_app.py
@@ -1,8 +1,15 @@
-import tensorflow as tf
+#importing libraries for image data handling
 import base64
 import numpy as np
 import io
 from PIL import Image
+
+#to ignore all the tensorflow related GPU warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+#importing libraries for handling the ML Model
+import tensorflow as tf
 import tensorflow.keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Sequential
@@ -10,21 +17,27 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import img_to_array
 
+#importing standard libraries for serving API
 from flask import request
 from flask import jsonify
 from flask import Flask
 
-session = tf.compat.v1.keras.backend.get_session()
-init = tf.compat.v1.global_variables_initializer()
-
+#to prevent any CORS policy error
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+#to prevent any CORS policy error
+CORS(app)
+
+#function to load the model into memory
 def get_model():
 	global model
-	model = load_model('cnn_model.h5')
-	print("* Model Loaded!")
+	model = load_model('model.h5')
+	print(" * Model Loaded!")
 
+#function to convert the image to a suitable format 
+#so that it can be plugged into the model
 def preprocess_image(image, target_size):
 	if image.mode != "RGB":
 		image = image.convert("RGB")
@@ -33,39 +46,34 @@ def preprocess_image(image, target_size):
 	image = np.expand_dims(image, axis = 0)
 
 	return image
-
-def pred(num):
-	if num == 1:
-		return "INFECTED"
-	return "NOT INFECTED"
-		
-tf.compat.v1.disable_eager_execution()
-
-print("* Loading Keras Model...")
+			
+print(" * Loading Keras Model...")
 graph = tf.compat.v1.get_default_graph()
 get_model()
 
+#our API endpoint that will service the frontend requests
 @app.route("/predict", methods=["POST"])	
 def predict():
+
+	#getting the encoded image from frontend 
 	message = request.get_json(force=True)
 	encoded = message['image']
+
+	#decoding the image and preprocessing it
 	decoded = base64.b64decode(encoded)
-	print("DECODED")
-	print(type(decoded))
-	byteimg = io.BytesIO(decoded);
-	print("BYTES")
-	print(byteimg)
-	image = Image.open(byteimg)
+	image = Image.open(io.BytesIO(decoded))
 	processed_image = preprocess_image(image, target_size=(64, 64))
+	test_datagen = ImageDataGenerator()
 
-	global graph
-	global session
-	with graph.as_default():
-		session.run(init)
-		prediction = model.predict(processed_image)
+	#preparing the input format for our Model
+	test_generator = test_datagen.flow(processed_image, [1], batch_size = 1)
+	
+	#predicting the presence of malaria
+	prediction = model.evaluate_generator(test_generator, steps = len(test_generator))
 
-		response = {
-			'prediction': pred(prediction[0][0])
-		}
+	response = {
+		'prediction': prediction[1]
+	}
 
-		return jsonify(response) 
+	#sending the prediction results back to frontend
+	return jsonify(response) 
